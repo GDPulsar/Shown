62dcff6492635f394cdead0d7c425e6a756d9c19
GDPulsar <jye.haddon@gmail.com>
2025-12-21 19:15:52 +1000

    gradle changes again

--- common/src/main/java/com/pulsar/shown/Shown.java
+++ common/src/main/java/com/pulsar/shown/Shown.java
@@ -1,12 +1,5 @@
 package com.pulsar.shown;
 
-import com.mojang.blaze3d.platform.InputConstants;
-import com.pulsar.shown.screen.SplitScreen;
-import com.pulsar.shown.screen.TabbedScreen;
-import dev.architectury.event.events.client.ClientTickEvent;
-import dev.architectury.platform.Platform;
-import dev.architectury.registry.client.keymappings.KeyMappingRegistry;
-import net.minecraft.client.KeyMapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
